f5526dfad186c31f5639d0bfe3036e8b3f0d3bf7
IndraP24 <indrashis985@gmail.com>
2021-07-15 20:50:52 +0530

    Initial additions

---
+++ backend/app/db/migrations/env.py
@@ -0,0 +1,42 @@
+from logging import config
+import pathlib
+import sys
+import alembic
+from sqlalchemy import engine_from_config, pool
+
+from logging.config import fileConfig
+import logging
+
+# we're appending the app directory to our path here so that we can import config easily
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[3]))
+
+from app.core.config import DATABASE_URL  # noqa
+
+# Alembic Config object, which provides access to values within the .ini file
+config = alembic.context.config
+
+# Interpret the config file for logging
+fileConfig(config.config_file_name)
+logger = logging.getLogger("alembic.env")
+
+def run_migrations_online() -> None:
+    """
+    Run migrations in 'online' mode
+    """
+
+    connectable = config.attributes.get("connection", None)
+    config.set_main_option("sqlalchemy.url", str(DATABASE_URL))
+    if connectable is None:
+        connectable = engine_from_config(
+            config.get_section(config.config_ini_section),
+            prefix="sqlalchemy.",
+            poolclass=pool.NullPool,
+        )
+        
+    with connectable.connect() as connection:
+        alembic.context.configure(
+            connection=connection,
+            target_metadata=None
+        )
+        with alembic.context.begin_transaction():
+            alembic.context.run_migrations()
